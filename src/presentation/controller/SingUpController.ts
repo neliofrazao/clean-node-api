1e9eb5f4ac3c09580ea3fed1b8e433de46b84624
Nelio Frazao <neliofrazac@gmail.com>
2020-03-31 08:43:45 -0300

    refactor: simplifies the code

--- src/presentation/controller/SingUpController.ts
+++ src/presentation/controller/SingUpController.ts
@@ -15,17 +15,12 @@ export class SingUpController implements Controller {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       const { email, password, passwordConfirmation } = httpRequest.body
       const isValid = this.emailValidator.isValid(email)
+
       for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
+        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
       }
-      if (password !== passwordConfirmation) {
-        return badRequest(new InvalidParamError('passwordConfirmation'))
-      }
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
+      if (password !== passwordConfirmation) return badRequest(new InvalidParamError('passwordConfirmation'))
+      if (!isValid) return badRequest(new InvalidParamError('email'))
     } catch (error) {
       return serverError()
     }
