02e1787006d5ccbae25b1c593c248efff5e222e1
Nelio Frazao <neliofrazac@gmail.com>
2020-08-12 14:31:38 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,6 +11,11 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   const makeSut = (): AccountMOngoRepository => {
     return new AccountMOngoRepository()
   }
